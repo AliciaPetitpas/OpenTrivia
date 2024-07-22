5e3256b3ba660dcde064f21439868af6ca8871e7
apetitpas2022 <alicia.petitpas@gmail.com>
2024-07-22 12:02:50 +0200

    Front

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { TitleComponent } from '../title/title.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, TitleComponent],
+  imports: [CommonModule, FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, TitleComponent],
 })
 export class HomePage {
+
+  myText: string = "a";
+
+  srcImg: string = "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+  altImg: string = "cat";
+
+  listFruits: any[] = ['papaye', 'mangue', 'pastèque'];
+
   constructor() {}
+
+  clickMe() {
+    alert("bouh");
+  }
 }
